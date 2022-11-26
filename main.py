4303e65aa5ff52746c09cb01c4be0711061b53ca
Muhammad <magomed.mirzoev.145@mail.ru>
2022-11-26 22:56:30 +0300

    work commit2

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtGui import QPainter, QColor, QPolygon
 import sys
 import random
 from UI import Ui_MainWindow
@@ -10,14 +10,14 @@ class Example(QMainWindow, Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
         self.flag = False
-        self.setWindowTitle('kirkle')
+        self.setWindowTitle('Супрематизм')
         self.pushButton.clicked.connect(self.draw)
         self.coords = []
 
     def draw(self):
         self.figure = 'circle'
-        self.size = random.randint(10, 300)
-        self.color = (255, 255, 0)
+        self.size = random.randint(10, 100)
+        self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
         self.flag = True
         self.update()
 
@@ -27,7 +27,7 @@ class Example(QMainWindow, Ui_MainWindow):
             qp.begin(self)
             qp.setPen(QColor(*self.color))
             qp.setBrush(QColor(*self.color))
-            self.x, self.y = random.randint(300, 300), random.randint(200, 200)
+            self.x, self.y = 300, 200
             if self.figure == 'circle':
                 qp.drawEllipse(self.x, self.y, self.size, self.size)
             qp.end()
