843385db112cad88eccf81df60590eb82622f5fd
Muhammad <magomed.mirzoev.145@mail.ru>
2022-11-26 22:38:23 +0300

    second repo

--- main.py
+++ main.py
@@ -1,15 +1,47 @@
-# This is a sample Python script.
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QPainter, QColor
+import sys
+import random
+from UI import Ui_MainWindow
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+SCREEN_SIZE = [680, 480]
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print('My first git-repo')
+class Example(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.flag = False
+        self.setWindowTitle('kirkle')
+        self.pushButton.clicked.connect(self.draw)
+        self.coords = []
 
-# Press the green button in the gutter to run the script.
+    def draw(self):
+        self.figure = 'circle'
+        self.size = random.randint(10, 300)
+        self.color = (255, 255, 0)
+        self.flag = True
+        self.update()
+
+    def paintEvent(self, event):
+        if self.flag:
+            qp = QPainter()
+            qp.begin(self)
+            qp.setPen(QColor(*self.color))
+            qp.setBrush(QColor(*self.color))
+            self.x, self.y = random.randint(300, 300), random.randint(200, 200)
+            if self.figure == 'circle':
+                qp.drawEllipse(self.x, self.y, self.size, self.size)
+            qp.end()
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app = QApplication(sys.argv)
+    sys.excepthook = except_hook
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec_())
